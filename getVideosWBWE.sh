5b5866b45fc255db65f0b8a87e801d15de5e29c0
frankforpresident <peters.frank@hotmail.com>
2016-11-11 15:58:43 +0100

    Add files via upload

---
+++ getVideosWBWE.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+#Created by Frank Peters
+#Date: 11/11/2016
+#
+
+#Download video assets from the SD card based on a folder with a date
+folders=($(curl -s {IP_ADDRESS}/VIDEO | grep -o '"fname":"[0-9]*"'));
+
+for i in ${folders[@]}
+do
+
+   echo "File" $i
+   DIR=($(echo $i | grep -o '"[0-9]*"' | sed -e 's/^"//' -e 's/"$//')); #sed -e workaround for [0-9*]
+   echo "Dir:" $DIR;
+   mkdir -p /shares/internal/CAM/$DIR/
+
+   directories=($(curl -s {IP_ADDRESS}/VIDEO/$DIR | grep -o '"fname":"[0-9]*.AVI"'));
+
+        for j in ${directories[@]}
+        do
+        files=$(echo "$j" | grep -o '[0-9]*.AVI');
+
+                #for every file in the current dir
+                for FILE in ${files[@]}
+                do
+                if [ ! -f /shares/internal/CAM/$DIR/$FILE ]
+                then
+                        #Download file from Flashair card because it doesn't exists
+                        echo "File:" $FILE "does not exists. Downloading...";
+                        wget http://{IP_ADDRESS}/VIDEO/$DIR/$FILE -P /shares/internal/CAM/$DIR/
+
+                else
+                        echo "File:" $FILE "exists. Skipping";
+                        #File Exists. DO NOTHING :)
+                fi
+                done
+        done
+done
+exit 0;
+
